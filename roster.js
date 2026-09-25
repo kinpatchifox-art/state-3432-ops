96818525ad5940fd8b289e60dd0260a585bbe1f2
kinpatchifox-art <kinpatchifox@gmail.com>
2026-09-25 01:27:07 +0100

    Add starter roster

---
+++ roster.js
@@ -0,0 +1,16 @@
+window.ROSTER = [
+{alliance:'MRG',name:'ジJOKERジ',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'MRG',name:'『ABU LAMAR』 KsA',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'MRG',name:'Fahad506',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'MRG',name:'Dracarys',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'AXE',name:'KHALIFA',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'AXE',name:'@SinDbaD',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'AXE',name:'صنديد',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'AXE',name:'SunLess',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'TOT',name:'BuAbdullah94',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'TOT',name:'Nanne',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'TOT',name:'Nuer515 KsA',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'TOT',name:"FaisaL'SA",infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'TOT',name:'SultanX UAE',infLvl:'T11',lanLvl:'T11',mmLvl:'T11'},
+{alliance:'TOT',name:'GROWTH',infLvl:'T11',lanLvl:'T10',mmLvl:'T11'}
+];
